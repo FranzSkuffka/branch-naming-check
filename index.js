bff1eca465cec65593a7e9f54c0e8a00c2e26dbf
FranzSkuffka <jottenwe@gmail.com>
2021-03-25 17:50:24 +0100

    Fix branch name check

--- index.js
+++ index.js
@@ -8,24 +8,22 @@ if (!process.argv[2]) {
   return;
 }
 
-currentBranchName().then(branchName => {
-  let validBranchNameRegExp;
-  try {
-    validBranchNameRegExp = new RegExp(process.argv[2], "g");
-  } catch (error) {
-    console.error(error.message + "\n");
-    process.exitCode = 1;
-    return;
-  }
-
-  if (validBranchNameRegExp.test(branchName)) {
-    process.exitCode = 0;
-  } else {
-    process.exitCode = 1;
-    console.error(
-      `Current Git branch name: "${branchName}" \n` +
-        `Doesn't match supplied RegExp: "${validBranchNameRegExp.toString()}"\n\n` +
-        `YOU HAVE BEEN REPORTED TO THE AUTHORITIES\n`
-    );
-  }
-});
+const branchName = currentBranchName()
+let validBranchNameRegExp;
+try {
+  validBranchNameRegExp = new RegExp(process.argv[2], "g");
+} catch (error) {
+  console.error(error.message + "\n");
+  process.exitCode = 1;
+  return;
+}
+if (validBranchNameRegExp.test(branchName)) {
+  process.exitCode = 0;
+} else {
+  process.exitCode = 1;
+  console.error(
+    `Current Git branch name: "${branchName}" \n` +
+      `Doesn't match supplied RegExp: "${validBranchNameRegExp.toString()}"\n\n` +
+      `YOU HAVE BEEN REPORTED TO THE AUTHORITIES\n`
+  );
+}
